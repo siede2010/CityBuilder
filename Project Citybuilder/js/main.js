f82b6b0f27d13042fb559876f70d5bf34eb7b6d5
siede2010 <63005632+siede2010@users.noreply.github.com>
2023-03-10 12:33:47 +0100

    Linked Other Buttons to their Screens

--- Project Citybuilder/js/main.js
+++ Project Citybuilder/js/main.js
@@ -1,7 +1,13 @@
 let m = getElementById(hidden,"mainMenue")
+let opt = getElementById(hidden,"optionScreen")
+let inf = getElementById(hidden,"infoScreen")
+let cre = getElementById(hidden,"creditScreen")
 
 let start = getElementById(m,"Start")
 let tutorial = getElementById(m,"Tutorial")
+let options = getElementById(m,"Options")
+let info = getElementById(m,"Info")
+let credits = getElementById(m,"Credits")
 class GlobalVar
 {
     constructor(startVar)
@@ -31,4 +37,27 @@ function main()
     
     start.addEventListener("click",() => {initiateGame()})
     tutorial.addEventListener("click",() => {initiateTutorial()})
+    options.addEventListener("click",() => {optionScreen()})
+    info.addEventListener("click",() => {infoScreen()})
+    credits.addEventListener("click",() => {creditScreen()})
+}
+function goMain()
+{
+    document.body.innerText = ""
+    document.body.append(m)
+}
+function optionScreen()
+{
+    m.remove()
+    document.body.append(opt)
+}
+function infoScreen()
+{
+    m.remove()
+    document.body.append(inf)
+}
+function creditScreen()
+{
+    m.remove()
+    document.body.append(cre)
 }
